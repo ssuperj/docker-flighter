08b1bc0be30f520d087b57259bdcd9345ab52064
yalu <114964251+ya1u@users.noreply.github.com>
2023-02-01 12:51:45 +0900

    update mypage-passport

--- src/main/flighter-front/src/components/MyPageNav.tsx
+++ src/main/flighter-front/src/components/MyPageNav.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import styled from "styled-components";
 import Nav from 'react-bootstrap/Nav';
+import UserInfo from "./UserInfo";
+import ReserveInfo from "./ReserveInfo";
 
 const StyledWrap = styled.div`
-  margin: 50px;
+  margin-top: 80px;
   display: flex;
   justify-content: center;
 
@@ -27,24 +29,27 @@ function MyPageNav() {
   const nav2 = () => setNav(2);
   const nav3 = () => setNav(3);
   const nav4 = () => setNav(4);
-  console.log(nav);
   return (
-    <StyledWrap>
-      <Nav fill variant="tabs" defaultActiveKey="link-1" className="Nav">
-        <Nav.Item className="navItem">
-          <Nav.Link href="#userinfo" onClick={nav1} className="navlink" eventKey="link-1">내 정보</Nav.Link>
-        </Nav.Item>
-        <Nav.Item className="navItem">
-          <Nav.Link href="#reserveinfo" onClick={nav2} className="navlink" eventKey="link-2">내 예약</Nav.Link>
-        </Nav.Item>
-        <Nav.Item className="navItem">
-          <Nav.Link href="#수정" onClick={nav3} className="navlink" eventKey="link-3">회원수정</Nav.Link>
-        </Nav.Item>
-        <Nav.Item className="navItem">
-          <Nav.Link href="#탈퇴" onClick={nav4} className="navlink" eventKey="link-4">회원탈퇴</Nav.Link>
-        </Nav.Item>
-      </Nav>
-    </StyledWrap>
+    <div>
+      <StyledWrap>
+        <Nav fill variant="tabs" defaultActiveKey="link-1" className="Nav">
+          <Nav.Item className="navItem">
+            <Nav.Link href="#userinfo" onClick={nav1} className="navlink" eventKey="link-1">내 정보</Nav.Link>
+          </Nav.Item>
+          <Nav.Item className="navItem">
+            <Nav.Link href="#reserveinfo" onClick={nav2} className="navlink" eventKey="link-2">내 예약</Nav.Link>
+          </Nav.Item>
+          <Nav.Item className="navItem">
+            <Nav.Link href="#수정" onClick={nav3} className="navlink" eventKey="link-3">회원수정</Nav.Link>
+          </Nav.Item>
+          <Nav.Item className="navItem">
+            <Nav.Link href="#탈퇴" onClick={nav4} className="navlink" eventKey="link-4">회원탈퇴</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </StyledWrap>
+      {nav === 1 && <UserInfo />}
+      {nav === 2 && <ReserveInfo />}
+    </div>
   );
 }
 
