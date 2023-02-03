93a31e967b8bb672284de9aa39d223322112029a
yalu <114964251+ya1u@users.noreply.github.com>
2023-02-03 09:46:31 +0900

    update mypage-UserInfo

--- src/main/flighter-front/src/components/UserInfo.tsx
+++ src/main/flighter-front/src/components/UserInfo.tsx
@@ -46,31 +46,33 @@ function UserInfo() {
         <h1 className="title">USER INFO</h1><br />
         <form className="content">
           <table>
+            <tbody>
             <tr>
               <th><label>이 &nbsp; 름</label></th>
-              <td><input type="text" value={"홍길동"} /></td>
+              <td><input type="text" defaultValue={"홍길동"} /></td>
             </tr>
             <tr>
               <th><label>아이디</label></th>
-              <td><input type="text" value={"qwe123"} disabled /></td>
+              <td><input type="text" defaultValue={"qwe123"} disabled /></td>
             </tr>
             <tr>
               <th><label>비밀번호</label></th>
-              <td><input type="password" value={""} /></td>
+              <td><input type="password" defaultValue={""} /></td>
             </tr>
             <tr>
               <th><label>비밀번호 확인</label></th>
-              <td><input type="password" value={""} /></td>
+              <td><input type="password" defaultValue={""} /></td>
             </tr>
             <tr>
               <th><label>이메일</label></th>
-              <td><input type="password" value={""} /></td>
+              <td><input type="password" defaultValue={""} /></td>
             </tr>
+            </tbody>
+            
           </table>
           <Button 
             type="submit"
             style={{ backgroundColor: "var(--color-r-m)", border: "none", opacity: "0.8" }}
-            href="/flighter-front"
             variant="secondary">
               수정완료
             </Button>
