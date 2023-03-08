988b9890cc8abff480d878e9a3a0e55141c5aa97
ssuperj <poqwer95@naver.com>
2023-03-08 11:31:46 +0900

    Test jenkins

--- flighter-frontend/src/index.tsx
+++ flighter-frontend/src/index.tsx
@@ -3,17 +3,16 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-// import { Provider } from "react-redux";
+import { Provider } from "react-redux";
 import store from "./redux/store";
-import { ApolloProvider } from "@apollo/client";
+// import { ApolloProvider } from "@apollo/client";
 import client from "./client";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   // <ApolloProvider client={client}>
-  // {/* <Provider store={store}> */}
-  // <App />
-  <>123</>
-  // {/* </Provider> */}
+  <Provider store={store}>
+    <App />
+  </Provider>
   // </ApolloProvider>
 );
